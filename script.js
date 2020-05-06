8c2d1db3b452421f93bf46c394b69e9eaa56bd3e
Sam Eff <HeySamEff@gmail.com>
2020-05-06 14:55:44 -0700

    added a draw white and draw random color functionality

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const container = document.querySelector(".container");
 const COLOR_BLACK = 'rgb(0,0,0)';
 const COLOR_WHITE = 'rgb(255,255,255)';
+let gridSize = 16;
 
 container.style.cssText = "grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);";
 
 function createPixelGrid() {
-    for (let i = 1; i <= 16; i++) {
-        for (let j = 1; j <= 16; j++) {
+    for (let i = 1; i <= gridSize; i++) {
+        for (let j = 1; j <= gridSize; j++) {
             let pxl = document.createElement('div');
             pxl.style.cssText = "grid-column: " + j + " / span 1; grid-row: " + i + " / span 1; background-color: rgb(255,255,255);";
-            pxl.addEventListener('mouseenter', drawBlack);
+            pxl.addEventListener('mouseenter', drawRainbow);
             container.appendChild(pxl);
         }
     }
@@ -17,6 +18,19 @@ function createPixelGrid() {
 
 function drawBlack(e) {
     e.target.style.backgroundColor = COLOR_BLACK;
+}
+
+function drawWhite(e) {
+    e.target.style.backgroundColor = COLOR_WHITE;
+}
+
+function drawRainbow(e) {
+    e.target.style.backgroundColor = `rgb(${randomColorValue()},${randomColorValue()},${randomColorValue()})`;
+}
+
+function randomColorValue() {
+    let val = Math.floor(Math.random() * 255);
+    return val;
 }
 
 createPixelGrid();
